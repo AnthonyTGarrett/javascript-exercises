0b052a5284770c57ad03728e728eec6d59252e98
Anthony Garrett <tony.garrett@gmail.com>
2022-10-24 17:29:02 -0400

    section completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
 
+    let arr = args.shift();
+
+    args.forEach(el => {
+        if(arr.indexOf(el) != -1){
+            arr.splice(arr.indexOf(el),1);
+        }
+        
+    })
+    
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
 
+    if(first < 0 || second < 0)
+        return 'ERROR'
+
+    if(typeof first != 'number' || typeof second != 'number')
+        return 'ERROR'
+        
+    let sum = 0;
+
+    let low = Math.min(first,second)
+    let high = Math.max(first,second)
+
+    for(let i = low; i <= high; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
